ad41fa74d2c9f106eb1d151d8513bd17c9df9156
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-07-15 16:52:13 +0700

    update readme

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\PersonController;
-use App\Http\Controllers\API\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,15 +19,6 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
-
-// ...existing code...
-Route::get('/people', [PersonController::class, 'index'])->name('people.index');
-Route::get('/people/create', [PersonController::class, 'create'])->name('people.create');
-Route::post('/people', [PersonController::class, 'store'])->name('people.store');
-Route::get('/people/edit/{user_id}', [PersonController::class, 'edit'])->name('people.edit');
-Route::put('/people/{user_id}', [PersonController::class, 'update'])->name('people.update');
-Route::delete('/people/{user_id}', [PersonController::class, 'destroy'])->name('people.destroy');
-Route::post('/people/{user_id}/archive-posts', [PersonController::class, 'archivePosts'])->name('people.archivePosts');
 
 
 
